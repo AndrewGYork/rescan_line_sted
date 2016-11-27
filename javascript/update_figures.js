875f056113d79fd27f8b673ea2dd8041815bae51
AndrewGYork <andrew.g.york+github@gmail.com>
2016-11-27 02:33:39 -0800

    Add files via upload

--- javascript/update_figures.js
+++ javascript/update_figures.js
@@ -14,39 +14,39 @@ img.src = big_image_directory + "/figure_1/point_1p00exc_9p00dep_008samps_001pul
 
 // Now that we know where our images are, we can load them:
 function update_figure_1() {
-  var exc = document.getElementById("Figure 1 excitation").value;
-  var dep = document.getElementById("Figure 1 depletion").value;
-  var smp = document.getElementById("Figure 1 samples").value;
-  var pul = document.getElementById("Figure 1 pulses").value;
-  var shp = document.getElementById("Figure 1 shape").value;
+  var exc = document.getElementById("Figure_1_excitation").value;
+  var dep = document.getElementById("Figure_1_depletion").value;
+  var smp = document.getElementById("Figure_1_samples").value;
+  var pul = document.getElementById("Figure_1_pulses").value;
+  var shp = document.getElementById("Figure_1_shape").value;
   var filename = big_image_directory + "/figure_1/" + shp + exc + dep + smp + pul + ".svg";
-  var image = document.getElementById("Figure 1 image");
+  var image = document.getElementById("Figure_1_image");
   image.src = filename;
 }
 
 function update_figure_2() {
-  var res = document.getElementById("Figure 2 resolution").value;
-  var img = document.getElementById("Figure 2 image").value;
-  var scn = document.getElementById("Figure 2 scan type").value;
+  var res = document.getElementById("Figure_2_resolution").value;
+  var img = document.getElementById("Figure_2_image").value;
+  var scn = document.getElementById("Figure_2_scan_type").value;
   var filename = big_image_directory + "/figure_2/figure_2_" + img + res + scn;
-  var gif = document.getElementById("Figure 2 gif");
-  var svg = document.getElementById("Figure 2 svg");
+  var gif = document.getElementById("Figure_2_gif");
+  var svg = document.getElementById("Figure_2_svg");
   gif.src = filename.concat(".gif");
   svg.src = filename.concat(".svg");
 }
 
 function update_figure_3() {
-  var mth = document.getElementById("Figure 3 imaging method").value;
-  var res = document.getElementById("Figure 3 resolution").value;
-  var img = document.getElementById("Figure 3 test image").value;
-  var fov = document.getElementById("Figure 3 field of view").value;
-  var num = document.getElementById("Figure 3 number of angles").value;
+  var mth = document.getElementById("Figure_3_imaging_method").value;
+  var res = document.getElementById("Figure_3_resolution").value;
+  var img = document.getElementById("Figure_3_test_image").value;
+  var fov = document.getElementById("Figure_3_field_of_view").value;
+  var num = document.getElementById("Figure_3_number_of_angles").value;
   var filename = big_image_directory + "/figure_3/" + mth;
   if ((mth === 'descan_line_') || (mth === 'rescan_line_')) {
     filename = filename.concat(num);
   }
   filename = filename.concat(img + fov + res + ".mp4");
   console.log(filename);
-  var vid = document.getElementById("Figure 3 vid");
+  var vid = document.getElementById("Figure_3_vid");
   vid.src = filename;
 }
